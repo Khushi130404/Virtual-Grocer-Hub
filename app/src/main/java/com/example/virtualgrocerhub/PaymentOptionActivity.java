dd315de5a68311da5044d84b08f5a62ca0e5f935
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-05-30 16:23:05 +0530

    Action Listener on Payment Methods

--- app/src/main/java/com/example/virtualgrocerhub/PaymentOptionActivity.java
+++ app/src/main/java/com/example/virtualgrocerhub/PaymentOptionActivity.java
@@ -3,6 +3,8 @@ package com.example.virtualgrocerhub;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class PaymentOptionActivity extends Activity
 {
     TextView tvAmount;
+    LinearLayout llDebit, llGpay, llNet, llPhone, llCash;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -22,5 +25,57 @@ public class PaymentOptionActivity extends Activity
 
         tvAmount = findViewById(R.id.tvAmount);
         tvAmount.setText(tvAmount.getText().toString()+getIntent().getIntExtra("amount",0));
+
+        llDebit = findViewById(R.id.llDebit);
+        llGpay = findViewById(R.id.llGpay);
+        llCash = findViewById(R.id.llCash);
+        llPhone = findViewById(R.id.llPhone);
+        llNet = findViewById(R.id.llNet);
+
+        llDebit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getApplicationContext(), FinalPaymentActivity.class);
+                startActivity(i);
+            }
+        });
+
+        llNet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getApplicationContext(), FinalPaymentActivity.class);
+                startActivity(i);
+            }
+        });
+
+        llPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getApplicationContext(), FinalPaymentActivity.class);
+                startActivity(i);
+            }
+        });
+
+        llCash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getApplicationContext(), FinalPaymentActivity.class);
+                startActivity(i);
+            }
+        });
+
+        llGpay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getApplicationContext(), FinalPaymentActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
