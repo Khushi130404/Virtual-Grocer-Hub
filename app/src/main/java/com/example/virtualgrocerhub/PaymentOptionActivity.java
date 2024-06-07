9ae9f7848eb3b027c7c25ab2875f9d2b5dac474c
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-06-07 10:17:07 +0530

    Payment

--- app/src/main/java/com/example/virtualgrocerhub/PaymentOptionActivity.java
+++ app/src/main/java/com/example/virtualgrocerhub/PaymentOptionActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,10 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 public class PaymentOptionActivity extends Activity
 {
     TextView tvAmount;
     LinearLayout llDebit, llGpay, llNet, llPhone, llCash;
+    ImageView imgPayMethod;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -31,6 +36,13 @@ public class PaymentOptionActivity extends Activity
         llCash = findViewById(R.id.llCash);
         llPhone = findViewById(R.id.llPhone);
         llNet = findViewById(R.id.llNet);
+        imgPayMethod = findViewById(R.id.imgPayMethod);
+
+        Glide.with(this)
+                .asGif()
+                .load(R.raw.pay2)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(imgPayMethod);
 
         llDebit.setOnClickListener(new View.OnClickListener()
         {
